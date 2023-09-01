52ccdf6f33ee05ded0412619c7bf17db207d07dd
Eduardo Tarouco <110120131+Edukd@users.noreply.github.com>
2023-09-01 15:31:23 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,46 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome} foi matriculado na formação $nome")
+    }
+
+    fun desmatricular(usuario: Usuario) {
+        inscritos.remove(usuario)
+        println("${usuario.nome} foi desmatriculado na formação $nome")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 60)
+    val conteudo2 = ConteudoEducacional("Banco de Dados", 90)
+
+    
+    val formacao = Formacao("Formação em Desenvolvimento Web", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2))
+
+    
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+
+    
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    
+    println("Inscritos na formação ${formacao.nome}: ${formacao.inscritos.map { it.nome }}")
+
+    
+    formacao.desmatricular(usuario1)
+
+    
+    println("Inscritos na formação ${formacao.nome}: ${formacao.inscritos.map { it.nome }}")
 }
+
